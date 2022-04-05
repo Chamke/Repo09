e1bbd6568041e39850d3f7265a9d445cac27b017
Chamke <terzimehic.amil@hotmail.com>
2022-04-06 00:07:51 +0200

    pushed

---
+++ ex18/src/index.js
@@ -0,0 +1,29 @@
+function myForLoop1(){
+    var evenNumbers = "" ;
+for(i = 0; i <= 8; i++){
+    if( i % 2 === 0){
+       (i === 8)? evenNumbers += i : evenNumbers += i + ", ";
+    }
+    
+}
+return evenNumbers;
+}
+function myForLoop2(){
+    var evenInverseNumbers = "";
+    for (i = 8; i >= 0; i--){
+        if(i % 2 === 0){
+            (i === 0) ? evenInverseNumbers += i : evenInverseNumbers += i + ", "
+        }
+    }
+    return evenInverseNumbers;
+}
+
+
+
+console.log(myForLoop1());
+console.log(myForLoop2());
+
+module.exports = {
+    myForLoop1,
+    myForLoop2
+};
